2d60aecd6c43a57ec6c8f1f05eddb5ea8866beca
Xargin <buaa.cch@live.com>
2020-06-12 11:36:07 +0800

    f

--- main.go
+++ main.go
@@ -18,7 +18,8 @@ func main() {
 
 		switch {
 		case strings.HasPrefix(cmd, addClusterCmd+" "):
-			clusterName := strings.TrimLeft(cmd, addClusterCmd+" ")
+			fmt.Println(cmd)
+			clusterName := strings.Split(cmd, " ")[1]
 			enterCluster(clusterName)
 		case strings.HasPrefix(cmd, addRouterCmd+" "):
 			routerName := strings.TrimLeft(cmd, addRouterCmd+" ")
@@ -38,9 +39,20 @@ const (
 	addListenerCmd = "addlistener"
 )
 
+type clusterConfig struct{}
+
 func enterCluster(clusterName string) {
-	clusterCompleter := func(d prompt.Document) []prompt.Suggest { return nil }
-	clusterPrompt := prompt.New(nil, clusterCompleter, prompt.OptionPrefix(fmt.Sprintf(`cluster %v >`, clusterName)))
+	clusterCompleter := func(d prompt.Document) []prompt.Suggest {
+		s := []prompt.Suggest{
+			{Text: "hostadd", Description: "hostadd [ip:port]"},
+			{Text: "lb_type LB_RANDOM", Description: "LB_RANDOM"},
+			{Text: "lb_type LB_ROUNDROBIN", Description: "LB_ROUNDROBIN"},
+		}
+		return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
+	}
+
+	clusterPrompt := prompt.New(nil, clusterCompleter, prompt.OptionPrefix(fmt.Sprintf(`.. CLUSTER[%v]) `, clusterName)))
+
 	for {
 		cmd := clusterPrompt.Input()
 		fmt.Println(cmd)
